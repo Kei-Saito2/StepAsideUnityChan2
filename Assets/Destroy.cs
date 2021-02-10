587929f0d3413eb8b4e9998f0cc9bf81ca61cf31
Kei-Saito2 <77094528+Kei-Saito2@users.noreply.github.com>
2021-02-10 17:28:18 +0900

    Initial commit

---
+++ Assets/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//課題
+
+public class Destroy : MonoBehaviour
+{
+    private GameObject unitychan;
+
+    void Start()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+
+    void Update()
+    {
+        if (transform.position.z < unitychan.transform.position.z - 7.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
